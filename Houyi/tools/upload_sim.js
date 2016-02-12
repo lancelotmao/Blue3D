81ca5a28f5454372c09051bc2e8f095a61dc7f80
Lance <lancelotmao@outlook.com>
2016-02-12 17:25:26 +0800

    add target to upload fmwk to server

---
+++ Houyi/tools/upload_sim.js
@@ -0,0 +1,33 @@
+/*
+ * test for http
+ */
+var http = require('http');
+var fs = require('fs');
+
+// var HOST = '127.0.0.1';
+var HOST = '120.25.219.17';
+
+var options = {
+    host: HOST,
+    port: 8888,
+    path: '/upload?projectId=Houyi_sim&type=fmwk',
+    method: 'POST'
+};
+
+var req = http.request(options, function(res) {
+  	console.log('STATUS: ' + res.statusCode);
+ 	  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  	res.setEncoding('utf8');
+  	res.on('data', function (chunk) {
+    	console.log('BODY: ' + chunk);
+  	});
+});
+
+var stream = fs.createReadStream('Houyi_sim.fmwk');
+stream.on('data', function(data) {
+  req.write(data);
+});
+
+stream.on('end', function() {
+  req.end();
+});
