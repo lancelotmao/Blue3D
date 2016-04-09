6e3496c0fd4b1702598c9bf09c64b1aa11069b64
Lance <lancelotmao@outlook.com>
2016-04-09 17:51:45 +0800

    revert code

--- Houyi/jni/Application.mk
+++ Houyi/jni/Application.mk
@@ -1,10 +1,10 @@
 APP_STL := stlport_static
 APP_OPTIM := debug
 APP_PLATFORM := android-18
-APP_ABI := armeabi
+APP_ABI := armeabi-v7a
 
 # Enable NEON
-TARGET_ARCH_ABI=armeabi
+TARGET_ARCH_ABI=armeabi-v7a
 
 NDK_TOOLCHAIN_VERSION := 4.9
 
